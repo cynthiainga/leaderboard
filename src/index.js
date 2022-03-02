804c805d79cdc417301ba899e838096a5bca94d6
Cynthia Inga <candyinga67@gmail.com>
2022-03-02 14:45:18 +0200

    Add renderScore function in index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import './css/style.css';
+import "./css/style.css";
+import getResponse, { getScore } from "./modules/api.js";
+
+const renderScore = (score) => {
+  const scoresList = document.querySelector(".scores-list");
+  scoresList.innerHTML = "";
+  score.forEach(({ user, score }) => {
+    const scoreItem = document.createElement("li");
+    scoreItem.classList.add("score-item");
+    scoreItem.innerHTML = `<span>${user}</span>: <span>${score}</span>`;
+    scoresList.appendChild(scoreItem);
+  });
+};
