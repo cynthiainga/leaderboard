b42abe5c051021381373174e1818597bdd8546a4
Cynthia Inga <candyinga67@gmail.com>
2022-03-02 14:59:05 +0200

    Add the event submit button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import "./css/style.css";
-import getResponse, { getScore } from "./modules/api.js";
+import './css/style.css';
+import getResponse, { getScore } from './modules/api.js';
 
 const renderScore = (score) => {
-  const scoresList = document.querySelector(".scores-list");
-  scoresList.innerHTML = "";
+  const scoresList = document.querySelector('.scores-list');
+  scoresList.innerHTML = '';
   score.forEach(({ user, score }) => {
-    const scoreItem = document.createElement("li");
-    scoreItem.classList.add("score-item");
+    const scoreItem = document.createElement('li');
+    scoreItem.classList.add('score-item');
     scoreItem.innerHTML = `<span>${user}</span>: <span>${score}</span>`;
     scoresList.appendChild(scoreItem);
   });
+};
+
+const form = document.querySelector('#score-form');
+const inputs = form.querySelectorAll('input');
+const refreshBtn = document.querySelector('.refresh-btn');
+
+refreshBtn.addEventListener('click', async () => {
+  const result = await getScore();
+  const { result: data } = await result.json();
+  renderScore(data);
+});
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const newPlayer = {
+    user: inputs[0].value,
+    score: inputs[1].value,
+  };
+  getResponse(newPlayer);
+});
+
+window.onload = async () => {
+  const result = await getScore();
+  const { result: data } = await result.json();
+  renderScore(data);
 };
